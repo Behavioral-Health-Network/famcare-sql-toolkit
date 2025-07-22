4dfefc919a159f75e284e186a7fac6a5831144fc
Bradley Wing <bwing@bhnstl.org>
2025-07-22 08:06:11 -0500

    Commits Clients-With-Multiple-Active-Pathways code and doc files.

---
+++ code/exception-reports/Clients-With-Multiple-Active-Pathways.sql
@@ -0,0 +1,11 @@
+/*
+Report Name: Clients With Multiple Active Pathways
+Category: Exception Reports
+
+See docs/exception-reports/Clients-With-Multiple-Active-Pathways.md for full documentation.
+*/
+
+SELECT *
+FROM BEHAVHEALT_LIVE.DBO.Q_CLIENTS_WITH_MULTIPLE_ACTIVE_PATHWAYS
+ORDER BY [COUNT_PATHWAY] DESC, 
+	CLIENT_NUMBER;
