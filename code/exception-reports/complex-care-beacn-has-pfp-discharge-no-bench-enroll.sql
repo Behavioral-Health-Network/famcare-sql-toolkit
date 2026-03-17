311100b19089922fc217b6abbf8cf062559e3037
Bradley Wing <bwing@bhnstl.org>
2026-03-17 14:06:37 -0500

    complex-care-beacn-has-pfp-discharge_no-bench-enroll: Adds initial commit of code and doc files.

---
+++ code/exception-reports/complex-care-beacn-has-pfp-discharge-no-bench-enroll.sql
@@ -0,0 +1,9 @@
+/*
+Report Name: Complex Care BEACN Has PfP Discharge No Bench Enroll
+Category: Exception Reports  
+
+See docs/exception-reports/complex-care-beacn-has-pfp-discharge-no-bench-enroll.md for full documentation.
+*/
+
+SELECT *
+FROM Q_COMPLEX_CARE_HAS_PFP_DISCHARGE_BUT_NO_BENCHMARK_ENROLLMENT
