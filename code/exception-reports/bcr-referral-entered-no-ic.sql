2455a26bc0c677c328b821246826452a291fe382
Bradley Wing <bwing@bhnstl.org>
2026-04-22 13:15:44 -0500

    q-bcr-referral-entered-no-ic: Adds initial commit of code and doc files.

---
+++ code/exception-reports/bcr-referral-entered-no-ic.sql
@@ -0,0 +1,9 @@
+/*
+Report Name: BCR Referrals Entered with No Initial Contact Exception Report  
+Category: Exception Reports  
+
+See docs/exception-reports/bcr-referrals-entered-no-initial-contact.md for full documentation.
+*/
+
+SELECT *
+FROM Q_BCR_REFERRAL_ENTERED_NO_IC
